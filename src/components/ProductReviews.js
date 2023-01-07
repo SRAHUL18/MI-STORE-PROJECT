dc205eae4f1c1480d7af1f07db15ef5bd2a6e044
Rahul <insiderahul18gmail.com>
2023-01-07 19:55:35 +0530

    initial commit

---
+++ src/components/ProductReviews.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ProductReviewCard from "./ProductReviewCard.js";
+import "../styles/ProductReviews.css";
+
+const ProductReviews = ({ productReviews }) => {
+  return (
+    <div className="ProductReviews">
+      {productReviews.map((item, index) => (
+        <ProductReviewCard
+          key={item.image}
+          index={index}
+          price={item.price}
+          name={item.name}
+          review={item.review}
+          image={item.image}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProductReviews;
---
+++ src/components/StarProducts.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/StarProduct.css";
+
+const StarProducts = ({ starProduct }) => {
+  return (
+    <div className="starProduct">
+      <div>
+        <a href={starProduct[0].url}>
+          {" "}
+          <img src={starProduct[0].image} alt="1 st Product" />
+        </a>
+      </div>
+      <div>
+        <a href={starProduct[1].url}>
+          <img src={starProduct[1].image} alt="2 nd Product"></img>
+        </a>
+        <a href={starProduct[2].url}>
+          <img src={starProduct[2].image} alt="3 rd Product"></img>
+        </a>
+        <a href={starProduct[3].url}>
+          <img src={starProduct[3].image} alt="4 th Product"></img>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default StarProducts;
